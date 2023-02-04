df4e4ba3a9524f532984ad603f6ab4fbf121581f
HyunDDeung <gustmd417@gmail.com>
2023-02-05 00:48:00 +0900

    Feat: "구현: 메인화면 리사이클러뷰"
    mainScreen의 도서 목록을 나타내는 recyclerView 구현

---
+++ app/src/main/java/com/example/majorbookservice/Data/DTO/MajorBooks.kt
@@ -0,0 +1,9 @@
+package com.example.majorbookservice.Data.DTO
+
+/** 암시 데이터셋을 이용하여 결과값 확인하기 */
+
+data class MajorBooks(
+    val className : String,
+    val professorName : String,
+    val classMajor: String
+)
