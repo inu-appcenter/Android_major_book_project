82c0efb2bc2e17175a25a818197c055c42a40cdb
HyunDDeung <gustmd417@gmail.com>
2023-02-05 00:51:52 +0900

    Feat: "구현: viewBinding"
    main_activity viewBinding 추가

--- app/src/main/java/com/example/majorbookservice/MainActivity.kt
+++ app/src/main/java/com/example/majorbookservice/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.majorbookservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.majorbookservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /** test */
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        R.layout.activity_main_screen
+        /*supportFragmentManager
+            .beginTransaction()
+            .replace(binding.layout.id, MainScreenFragment())
+            .commit()
+*/
     }
 }
