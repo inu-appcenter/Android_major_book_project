c571be4cc5fbb03a976c1d18bd818090cd28d882
HyunDDeung <gustmd417@gmail.com>
2023-02-09 02:43:32 +0900

    Feat: "spinner 구현"
    기존의 textInputLayout spinner를 오픈소스를 이용한 powerspinner로 교체했습니다

--- app/src/main/java/com/example/majorbookservice/MainScreenActivity.kt
+++ app/src/main/java/com/example/majorbookservice/MainScreenActivity.kt
@@ -2,38 +2,40 @@ package com.example.majorbookservice
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.majorbookservice.Data.DTO.MajorBooks
+import com.example.majorbookservice.UI.adapter.Adapter
 import com.example.majorbookservice.databinding.ActivityMainScreenBinding
 
 
 class MainScreenActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainScreenBinding
+    val mDatas=mutableListOf<MajorBooks>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= DataBindingUtil.setContentView(this, R.layout.activity_main_screen)
         setupAutoCompleteView()
 
-       /* binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                binding.textView.text = "click x"
-
+                /** 검색버튼에 사용할 액션들 구현 */
                 return true
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
+                binding.textHint.visibility = View.GONE
                 return true
             }
 
-        })*/
+        })
 
 
         binding.searchView.setOnCloseListener(object : SearchView.OnCloseListener {
@@ -41,7 +43,6 @@ class MainScreenActivity : AppCompatActivity() {
                 softkeyboardHide()
                 binding.searchView.onActionViewCollapsed()
                 binding.textHint.visibility = View.VISIBLE
-                //binding.textView.text = "click O"
                 return true
             }
         })
@@ -50,19 +51,44 @@ class MainScreenActivity : AppCompatActivity() {
         binding.searchView.setOnSearchClickListener {
             binding.textHint.visibility = View.GONE
         }
+
+        /** RecyclerView Test를 위해 사용할 데이터 셋.
+         *
+         * 추후 삭제 예정
+         *
+         * */
+        initDogRecyclerView()
+        initializelist()
+
+
+        /** Spinner 아이템 상태에 따른 layout 변경 */
+        binding.spinnerView.setOnClickListener {
+            binding.spinnerView.show()
+            binding.spinnerView.setBackgroundResource(R.drawable.spinner_main_background_opened)
+            //binding.spinnerView.setPadding(12, 8, 0, 5)
+            binding.spinnerView.setPaddingRelative(12, 1, 5, 0)
+        }
+
+        binding.spinnerView.apply {
+            setOnSpinnerItemSelectedListener<String> { _, _, _, item ->
+                binding.spinnerView.setBackgroundResource(R.drawable.spinner_main_background_closed)
+                binding.spinnerView.setPaddingRelative(12, 1, 5, 0)
+            }
+        }
+
     }
 
     /** spinner 내부에 들어갈 데이터 추가 및 연결 */
     private fun setupAutoCompleteView() {
         val dataList=getDataList()
         val adapter: ArrayAdapter<String> = ArrayAdapter<String>(this,R.layout.spinner_item_mainscreen, dataList )
-        binding.spinnerFilter.setAdapter(adapter)
+
+        /*binding.spinnerFilter.setAdapter(adapter)*/
        /* mContentViewBinding.spinnerFilter.onItemClickListener =
             AdapterView.OnItemClickListener { parent, arg1, position, id ->
                 //TODO: You can your own logic.
                 // onClick에 관련된 이벤트 추가로 구현 가능
             }*/
-
     }
     /** 키보드 내리기 */
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -83,4 +109,26 @@ class MainScreenActivity : AppCompatActivity() {
         dataList.add("학과명")
         return dataList
     }
+
+    /** 리사이클러뷰 화면 실행 */
+    fun initDogRecyclerView(){
+        val adapter= Adapter() //어댑터 객체 만듦
+        adapter.datalist=mDatas //데이터 넣어줌
+        binding.recyclerView.adapter=adapter //리사이클러뷰에 어댑터 연결
+        binding.recyclerView.layoutManager= LinearLayoutManager(this) //레이아웃 매니저 연결
+    }
+
+    fun initializelist(){ //임의로 데이터 넣어서 만들어봄
+        with(mDatas){
+            add(MajorBooks("UI/UX", "Lee", "컴퓨터공학부"))
+            add(MajorBooks("UI/UX", "Lee", "컴퓨터공학부"))
+            add(MajorBooks("UI/UX", "Lee", "컴퓨터공학부"))
+            add(MajorBooks("UI/UX", "Lee", "컴퓨터공학부"))
+            add(MajorBooks("UI/UX", "Lee", "컴퓨터공학부"))
+            add(MajorBooks("UI/UX", "Lee", "컴퓨터공학부"))
+            add(MajorBooks("UI/UX", "Lee", "컴퓨터공학부"))
+            add(MajorBooks("UI/UX", "Lee", "컴퓨터공학부"))
+        }
+    }
+
 }
