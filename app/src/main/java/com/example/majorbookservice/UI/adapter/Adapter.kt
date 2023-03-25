18a5e6b850e622f7c82ae4466159cf80f7804149
HyunDDeung <gustmd417@gmail.com>
2023-03-25 22:32:52 +0900

    Ignore

--- app/src/main/java/com/example/majorbookservice/UI/adapter/Adapter.kt
+++ app/src/main/java/com/example/majorbookservice/UI/adapter/Adapter.kt
@@ -1,6 +1,7 @@
 package com.example.majorbookservice.UI.adapter
 
 import android.content.ClipData
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
